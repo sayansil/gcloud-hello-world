7deeebb78a4fcc9b0b2dad4af0b325258a03c961
Sayan Sil <sayan227sil@ymail.com>
2019-03-05 00:03:48 +0530

    Get server time

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,9 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/all-text')
+def server_time():
+    return render_template(
+        'server_time.html',
+        time=str(datetime.now()))
+
 @app.route('/submission', methods=['POST'])
 def submission():
     name = request.form['name']
     return render_template(
         'submission.html',
         name=name)
+
+
