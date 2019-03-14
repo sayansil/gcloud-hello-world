9e331755798a5b94c681c94f7d037e11a2c581e6
Sayan Sil <sayan227sil@ymail.com>
2019-03-14 17:35:42 +0530

    Update unittest to use existing model

--- local_unittests/ndb_test.py
+++ local_unittests/ndb_test.py
@@ -1,11 +1,7 @@
 import unittest
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
-
-class TestModel(ndb.Model):
-    text = ndb.StringProperty()
-    timestamp = ndb.DateTimeProperty(auto_now_add=True)
-    tags = ndb.StringProperty(repeated=True)
+from models.Entry import Entry
 
 class DatastoreTestCase(unittest.TestCase):
 
@@ -22,39 +18,39 @@ class DatastoreTestCase(unittest.TestCase):
 
     def testInsertEntity(self):
         text = 'Hello world', 'New world'
-        TestModel(text=text[0], tags=text[0].split()).put()
-        TestModel(text=text[1], tags=text[1].split()).put()
-        self.assertEqual(2, len(TestModel.query().fetch(3)))
+        Entry(text=text[0], tags=text[0].split()).put()
+        Entry(text=text[1], tags=text[1].split()).put()
+        self.assertEqual(2, len(Entry.get_all_entries()))
 
     def testExactSearch(self):
         text = 'Hello world', 'New world'
-        TestModel(text=text[0], tags=text[0].split()).put()
-        TestModel(text=text[1], tags=text[1].split()).put()
+        Entry(text=text[0], tags=text[0].split()).put()
+        Entry(text=text[1], tags=text[1].split()).put()
         search_text = 'Hello world'
-        results = TestModel.query(TestModel.text==search_text).fetch(2)
+        results = Entry.exact_search(search_text)
         self.assertEqual(1, len(results))
         self.assertEqual(search_text, results[0].text)
 
     def testKeywordSearch(self):
         text = 'Hello world', 'New world', 'Hello in ndb'
-        TestModel(text=text[0], tags=text[0].split()).put()
-        TestModel(text=text[1], tags=text[1].split()).put()
-        TestModel(text=text[2], tags=text[2].split()).put()
+        Entry(text=text[0], tags=text[0].split()).put()
+        Entry(text=text[1], tags=text[1].split()).put()
+        Entry(text=text[2], tags=text[2].split()).put()
         search_text = 'world'
-        results = TestModel.query(TestModel.tags.IN(search_text.split())).fetch(3)
+        results = Entry.keyword_search(search_text)
         self.assertEqual(2, len(results))
         search_text = 'ndb'
-        results = TestModel.query(TestModel.tags.IN(search_text.split())).fetch(3)
+        results = Entry.keyword_search(search_text)
         self.assertEqual(1, len(results))
         self.assertEqual('Hello in ndb', results[0].text)
 
     def testDeleteEntity(self):
         text = 'Hello world'
-        TestModel(text=text, tags=text.split()).put()
-        results = TestModel.query(TestModel.text==text).fetch(2)
+        Entry(text=text, tags=text.split()).put()
+        results = Entry.exact_search(text)
         self.assertEqual(1, len(results))
-        results[0].key.delete()
-        results = TestModel.query(TestModel.text==text).fetch(2)
+        results[0].delete_entry()
+        results = Entry.exact_search(text)
         self.assertEqual(0, len(results))
 
 if __name__ == '__main__':
